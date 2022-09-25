97f84c75ac7ba534369561c21f9b8db54ea7e8ba
jonaslng <jonasdelorean@gmail.com>
2022-09-25 21:25:42 +0200

    added cart & improved shop

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -4,10 +4,36 @@ import { useState } from 'react'
 import { RadioGroup } from '@headlessui/react'
 import { IconStar } from '@tabler/icons'
 import Navbar from "../_Navbar"
+import Link from 'next/link'
+const axios = require('axios').default;
+
+
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+const colorData = {
+  "blau": "bg-blue-500",
+  "grün": "bg-green-500",
+  "rot": "bg-red-500",
+  "gelb": "bg-yellow-400",
+  "schwarz": "bg-black",
+  "bordeaux": "bg-rose-900",
+  "lila": "bg-purple-800",
+  "orange": "bg-orange-500",
+  "hellblau": "bg-sky-400",
+  "weiß": "bg-white",
+  "rosa": "bg-pink-500",
+  "grau": "bg-gray-500",
+}
+
 
 export default function Product() {
     const router = useRouter()
     const reviews = { href: '#', average: 4, totalCount: 117 }
+    const [selectedColor,setSelectedColor] = useState("blau");
+    const [selectedSize,setSelectedSize] = useState("");
     const { id } = router.query
     let product = {};
     if(data !== undefined && id !== undefined) {
@@ -21,7 +47,9 @@ export default function Product() {
         images: data[id-1].images,
         description: data[id-1].description,
         highlights: data[id-1].highlights === undefined ? [] : data[id-1].highlights,
-        details: data[id-1].details === undefined ? "" : data[id-1].details
+        details: data[id-1].details === undefined ? "" : data[id-1].details,
+        variants: data[id-1].variants === undefined ? [] : data[id-1].variants,
+        discount: data[id-1].discount === undefined ? false : data[id-1].discount,
     }
     }
     console.log(product);
@@ -36,9 +64,9 @@ export default function Product() {
               {product.breadcrumbs.map((breadcrumb) => (
                 <div key={breadcrumb.id}>
                   <div className="flex items-center">
-                    <a href={breadcrumb.href} className="mr-2 text-sm font-medium text-gray-900">
+                    <Link href={breadcrumb.href}><a className="mr-2 text-sm font-medium text-gray-900">
                       {breadcrumb.name}
-                    </a>
+                    </a></Link>
                     <svg
                       width={16}
                       height={20}
@@ -95,24 +123,22 @@ export default function Product() {
             {/* Options */}
             <div className="mt-4 lg:row-span-3 lg:mt-0">
               <h2 className="sr-only">Product information</h2>
-              <p className="text-3xl tracking-tight text-gray-900">{product.price}</p>
+              <p className="text-3xl tracking-tight line-through text-gray-300">{product.discount === false ? "" : product.discount.priceStr}</p><p className="text-3xl tracking-tight text-gray-900">{product.price}</p>
               <p className='text-gray-400 text-l'>Endpreis, zzg. Versandkosten</p>
   
-              <form className="mt-10">
-                {/* Colors 
-                <div>
+                
+                {product.variants.includes("blau") ? <div>
                   <h3 className="text-sm font-medium text-gray-900">Color</h3>
   
                   <RadioGroup value={selectedColor} onChange={setSelectedColor} className="mt-4">
-                    <RadioGroup.Label className="sr-only"> Choose a color </RadioGroup.Label>
+                    <RadioGroup.Label className="sr-only"> Wähle eine Farbe </RadioGroup.Label>
                     <div className="flex items-center space-x-3">
-                      {product.colors.map((color) => (
+                      {product.variants.map((color) => (
                         <RadioGroup.Option
                           key={color.name}
                           value={color}
                           className={({ active, checked }) =>
                             classNames(
-                              color.selectedClass,
                               active && checked ? 'ring ring-offset-1' : '',
                               !active && checked ? 'ring-2' : '',
                               '-m-0.5 relative p-0.5 rounded-full flex items-center justify-center cursor-pointer focus:outline-none'
@@ -127,37 +153,32 @@ export default function Product() {
                             aria-hidden="true"
                             className={classNames(
                               color.class,
-                              'h-8 w-8 border border-black border-opacity-10 rounded-full'
+                              'h-8 w-8 border border-black border-opacity-10 rounded-full',
+                              colorData[color]
                             )}
                           />
                         </RadioGroup.Option>
                       ))}
                     </div>
                   </RadioGroup>
-                </div>
+                </div> : null}
+                
   
                 
-                <div className="mt-10">
+                {product.variants.length > 0 && !product.variants.includes("blau") ? <div className="mt-10">
                   <div className="flex items-center justify-between">
-                    <h3 className="text-sm font-medium text-gray-900">Size</h3>
-                    <a href="#" className="text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                      Size guide
-                    </a>
+                    <h3 className="text-sm font-medium text-gray-900">Größe</h3>
                   </div>
   
                   <RadioGroup value={selectedSize} onChange={setSelectedSize} className="mt-4">
-                    <RadioGroup.Label className="sr-only"> Choose a size </RadioGroup.Label>
+                    <RadioGroup.Label className="sr-only">Größe</RadioGroup.Label>
                     <div className="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
-                      {product.sizes.map((size) => (
+                      {product.variants.map((size) => (
                         <RadioGroup.Option
                           key={size.name}
                           value={size}
-                          disabled={!size.inStock}
                           className={({ active }) =>
-                            classNames(
-                              size.inStock
-                                ? 'bg-white shadow-sm text-gray-900 cursor-pointer'
-                                : 'bg-gray-50 text-gray-200 cursor-not-allowed',
+                            classNames('bg-white shadow-sm text-gray-900 cursor-pointer',
                               active ? 'ring-2 ring-indigo-500' : '',
                               'group relative border rounded-md py-3 px-4 flex items-center justify-center text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6'
                             )
@@ -165,8 +186,8 @@ export default function Product() {
                         >
                           {({ active, checked }) => (
                             <>
-                              <RadioGroup.Label as="span">{size.name}</RadioGroup.Label>
-                              {size.inStock ? (
+                              <RadioGroup.Label as="span">{size}</RadioGroup.Label>
+                              
                                 <span
                                   className={classNames(
                                     active ? 'border' : 'border-2',
@@ -175,37 +196,21 @@ export default function Product() {
                                   )}
                                   aria-hidden="true"
                                 />
-                              ) : (
-                                <span
-                                  aria-hidden="true"
-                                  className="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200"
-                                >
-                                  <svg
-                                    className="absolute inset-0 h-full w-full stroke-2 text-gray-200"
-                                    viewBox="0 0 100 100"
-                                    preserveAspectRatio="none"
-                                    stroke="currentColor"
-                                  >
-                                    <line x1={0} y1={100} x2={100} y2={0} vectorEffect="non-scaling-stroke" />
-                                  </svg>
-                                </span>
-                              )}
                             </>
                           )}
                         </RadioGroup.Option>
                       ))}
                     </div>
                   </RadioGroup>
-                </div>
-                */}
+                              </div> : null}
+                
   
                 <button
-                  type="submit"
                   className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  onClick={() => addToCart(data[id-1])}
                 >
-                  Add to bag
+                  Zum Warenkorb hinzufügen
                 </button>
-              </form>
             </div>
   
             <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pb-16 lg:pr-8">
@@ -245,4 +250,15 @@ export default function Product() {
       </div>}
       </>
       )
+}
+
+async function addToCart(product) {
+  console.log("ADD TO CART")
+  axios({
+    method: 'post',
+    url: '/api/addtocart',
+    data: product
+  }).then(res => {
+    console.log(res)
+  })
 }
--- pages/shop/_productList.tsx
+++ pages/shop/_productList.tsx
@@ -1,3 +1,5 @@
+import Link from "../../node_modules/next/link"
+
 const products = require("../../public/PRODUCTS.json")
   
 
@@ -12,7 +14,7 @@ const products = require("../../public/PRODUCTS.json")
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {products.map((product) => (
               <div key={product.id} className="group relative">
-                <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                <div className={product.discount === false ? "min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80" : "min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80"}>
                   <img
                     src={product.images[0]}
                     alt="Product"
@@ -22,10 +24,10 @@ const products = require("../../public/PRODUCTS.json")
                 <div className="mt-4 flex justify-between">
                   <div>
                     <h3 className="text-sm text-gray-700">
-                      <a href={product.href}>
+                      <Link href={product.href}><a>
                         <span aria-hidden="true" className="absolute inset-0" />
                         {product.name}
-                      </a>
+                      </a></Link>
                     </h3>
                   </div>
                   {product.discount !== false ? <p className="text-sm font-medium text-gray-500 line-through">{product.discount.priceStr}</p> : null}<p className="text-sm font-medium text-gray-900">{product.priceStr}</p>
